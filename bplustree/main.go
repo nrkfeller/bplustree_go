57fbdb05e374c8da334eb165f5206308a876a1ec
NickFeller <nrkfeller@gmail.com>
2016-06-03 23:19:28 -0400

    updated added tests

--- bplustree/main.go
+++ bplustree/main.go
@@ -5,7 +5,13 @@ import "fmt"
 func main() {
 	tree := newBTree(3)
 
-	tree.Insert([2,3,4])
+	keys := make(keys, 0, 3)
+
+	for i := 0; i < 3; i++ {
+		keys = append(keys, []Key{4})
+	}
+
+	tree.Insert(keys...)
 
 	fmt.Println(tree)
 }
